700b61dd922eae5cd6f79470ce7c2012a8b356a3
Kenta Nishimoto <0tb1161j@gmail.com>
2022-03-17 10:44:50 +0900

    設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,100 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.17.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "capistrano2"
+set :repo_url, 'https://github.com/kenta-nishimoto-1111/capistrano2.git'
+server "192.168.1.105", port: 2525, roles: [:app, :web, :db], primary: true
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+# user
+set :user,            'deploy'
+set :use_sudo,        false
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+# server
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/var/www/rails/#{fetch(:application)}"
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+# puma
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log,  "#{release_path}/log/puma.error.log"
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
 
-# Default value for :pty is false
-# set :pty, true
+# terminal
+set :pty,             true
 
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
+# ssh
+set :ssh_options,     {
+    user: 'deploy'
+}
 
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
+# rvm
+set :rbenv_ruby, '2.6.3'
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# environment
+set :linked_dirs, fetch(:linked_dirs, []).push(
+    'log',
+    'tmp/pids',
+    'tmp/cache',
+    'tmp/sockets',
+    'vendor/bundle',
+    'public/system',
+    'public/uploads'
+)
+set :linked_files, fetch(:linked_files, []).push(
+    'config/database.yml',
+    'config/secrets.yml'
+)
 
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
+namespace :puma do
+    desc 'Create Directories for Puma Pids and Socket'
+    task :make_dirs do
+        on roles(:app) do
+        execute "mkdir #{shared_path}/tmp/sockets -p"
+        execute "mkdir #{shared_path}/tmp/pids -p"
+        end
+    end
+    
+    before :deploy, :make_dirs
+end
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+namespace :deploy do
+    desc "Make sure local git is in sync with remote."
+    task :check_revision do
+        on roles(:app) do
+        unless `git rev-parse HEAD` == `git rev-parse origin/master`
+            puts "WARNING: HEAD is not the same as origin/master"
+            puts "Run `git push` to sync changes."
+            exit
+        end
+        end
+    end
 
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+    desc 'Restart application'
+    task :restart do
+        on roles(:app), in: :sequence, wait: 5 do
+        invoke 'puma:restart'
+        end
+    end
+
+    desc 'Upload database.yml and secrets.yml'
+    task :upload do
+        on roles(:app) do |host|
+        if test "[ ! -d #{shared_path}/config ]"
+            execute "mkdir -p #{shared_path}/config"
+        end
+        upload!('config/database.yml', "#{shared_path}/config/database.yml")
+        upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+        end
+    end
+
+    before :deploy,     :upload
+    before :deploy,     :check_revision
+end
